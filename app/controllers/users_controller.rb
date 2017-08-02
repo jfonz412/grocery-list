a314624a2931bc4f2abd17e2fb27418984ac9cc0
jfonz412 <jfonzone@outlook.com>
2017-08-02 13:30:23 -0400

    sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Thanks for signing up!"
+  		redirect_to root_url
+  	else
+  		flash.now[:warning] = "Something went wrong..."
+  		render users_new_path
+  	end
   end
 
   def show
   end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password,
+  																 :password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  'users/new'
+  post 'users/new', to: 'users#create'
   get  'users/show'
   get 'static_pages/about'
 end
