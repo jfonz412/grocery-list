81ff23da28ee996bc290c7218252921f7a998ebb
jfonz412 <jfonzone@outlook.com>
2017-08-02 14:45:09 -0400

    signup tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,14 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get static_pages_about_path
     assert_response :success
   end
 
+  #if signed in, header should say log out
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.new(name: "Foobar", email: "Foo@bar.com",
+                     password: "pass123", password_confirmation: "pass123")
+  end
+
   test "should get new" do
     get users_new_url
     assert_response :success
@@ -13,6 +18,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should have proper title" do
   	get users_new_url
-  	assert_select "title", "New User"
+  	assert_select "title", "Sign Up"
+  end
+
+  test "signup with valid info" do
+    get users_new_path 
+    assert_difference "User.count", 1 do
+      post users_new_path, params:{ user: { name: "Foobar", email: "Foo@bar.com",
+                                            password: "pass123", 
+                                            password_confirmation: "pass123" } }
+    end
+  end
+
+  test "no signup with invalid info" do
+    get users_new_path 
+    assert_no_difference "User.count" do
+      post users_new_path, params:{ user: { name: " ", email: " ",
+                                            password: " ", 
+                                            password_confirmation: " " } }
+    end
   end
 end
