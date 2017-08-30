d838f77e2ab7c952a8e726a2699399137ed0ff2e
jfonz412 <jfonzone@outlook.com>
2017-08-30 12:28:07 -0400

    test new category dropdown is added

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
+
+	# Does not seem to be testing remote/JQuery submissions
   test "should create category if logged in" do
   	assert_no_difference "Category.count" do
     	post categories_create_url params: { category: {name: "Category"} }
+		end
+		log_in_as(users(:bob))
+	  	assert_difference "Category.count" do
+	    	post categories_create_url params: { category: {name: "Category"} }
+		end
+		get new_list_path
+		assert_select "div:match('id', ?)", "Category"
 	end
-	log_in_as(users(:bob))
-  	assert_difference "Category.count" do
-    	post categories_create_url params: { category: {name: "Category"} }
-	end
-  end
 end
