94d44b1d4ee4c71029ca06d71ada92a4bf63df64
jfonz412 <jfonzone@outlook.com>
2017-09-01 17:30:55 -0400

    remove user-item creation from list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,15 +3,13 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
-    create_new_item_form
-    create_new_category_form
+    @category = Category.new
   end
 
   def create
     @list = List.new(name: list_params[:name], user_id: current_user.id)
     if @list.save
     	build_list_items
-    	build_user_items
       flash[:success] = "List created!"
       redirect_to lists_show_path(@list)
     else
@@ -42,8 +40,6 @@ class ListsController < ApplicationController
   private
     def list_params
       params.require(:list).permit(:name, :list_item => [:user_item_id, :quantity],
-      																		:user_item => [:category_id, :name, 
-                                                         :price, :quantity],
                                           :category  => [:name])
     end
 
@@ -54,32 +50,4 @@ class ListsController < ApplicationController
   			end
   		end
   	end
-
-  	def build_user_items
-  		unless params[:list][:user_item].nil?
-  			params[:list][:user_item].each do |u|
-  				item = current_user.user_items.build(name: u[:name], price: u[:price], category_id: u[:category_id])
-          item.save
-          list_item = @list.list_items.build(user_item_id: item.id, quantity: u[:quantity])
-  			  if !list_item.save
-            flash[:warning] = list_item.errors.full_messages
-          end
-        end
-  		end
-  	end
-
-    def create_new_item_form
-      if params[:category_id] 
-        @category = Category.find(params[:category_id])
-        @list = List.new
-        render 'new.js.erb'
-      end
-    end
-
-    def create_new_category_form
-      if params[:new_category_form]
-        @category = current_user.categories.build
-        render 'new_category.js.erb'
-      end
-    end
 end
